9b826a2297f2a360058e13a306327fb68d8587d1
Andy Georges <itkovian@gmail.com>
2023-01-23 12:30:08 +0100

    bump: version to 2.0.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ import vsc.install.shared_setup as shared_setup
 from vsc.install.shared_setup import ag
 
 PACKAGE = {
-    'version': '1.3.1',
+    'version': '2.0.0',
     'author': [ag],
     'maintainer': [ag],
     'excluded_pkgs_rpm': ['vsc', 'vsc.filesystem', 'vsc.filesystem.quota'],
