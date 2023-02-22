f2eef80553615a30f5d302f4e176c0a87147e7d6
Andy Georges <itkovian@gmail.com>
2023-02-22 11:11:33 +0100

    fix: headers

--- lib/vsc/filesystem/quota/entities.py
+++ lib/vsc/filesystem/quota/entities.py
@@ -2,16 +2,27 @@
 #
 # Copyright 2023-2023 Ghent University
 #
-# This file is part of vsc-filesystems,
+# This file is part of vsc-filesystems-quota,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
 # with support of Ghent University (http://ugent.be/hpc),
 # the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
 # the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
-# https://github.com/hpcugent/vsc-filesystems
+# https://github.com/hpcugent/vsc-filesystems-quota
 #
-# All rights reserved.
+# vsc-filesystems-quota is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Library General Public License as
+# published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
+#
+# vsc-filesystems-quota is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with vsc-filesystems-quota. If not, see <http://www.gnu.org/licenses/>.
 #
 """Various classes that can represent quota information for the various
 devices an entity (user, vo, ...) uses on the VSC storage.
